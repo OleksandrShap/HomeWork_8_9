f0fed186255233bdde3aee4f85356c8b6d4dff72
OleksandrShap <moeee@ukr.net>
2025-06-14 21:34:19 +0300

    HomeWork_8
    Add:
    -Platforms
    -Player
    -Two scripts for player (MovePlayer, PlayerWrapAround)

---
+++ Assets/HomeWork8_9/Scripts/Runtime/Player/PlayerWrapAround.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class PlayerWrapAround : MonoBehaviour
+{
+    [SerializeField] private float _buffer = 0.5f;
+
+    private float _leftLimit;
+    private float _rightLimit;
+    private float _topLimit;
+    private float _botLimit;
+
+    private void Start()
+    {
+        var camera = Camera.main;
+
+        Vector3 leftPoint = camera.ViewportToWorldPoint(new Vector3(0, 0, 0));
+        Vector3 rightPoint = camera.ViewportToWorldPoint(new Vector3(1, 0, 0));
+        
+        _leftLimit = leftPoint.x - _buffer;
+        _rightLimit = rightPoint.x + _buffer;
+        
+    }
+
+    private void Update()
+    {
+        CheckwrapAround();
+    }
+
+    private void CheckwrapAround()
+    {
+        Vector3 bufferPosition = transform.position;
+        if (bufferPosition.x < _leftLimit)
+        {
+            bufferPosition.x = _rightLimit;
+        }
+        else if (bufferPosition.x > _rightLimit)
+        {
+            bufferPosition.x = _leftLimit;
+        }
+        else if (bufferPosition.y < -7.76f)
+        {
+            bufferPosition.y = 6.43f;
+        }
+        transform.position = bufferPosition;
+    }
+}
